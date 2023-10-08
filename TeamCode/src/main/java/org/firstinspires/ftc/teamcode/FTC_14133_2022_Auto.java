9a52223aca08a9f17d82d846a4ef281ab46b2a32
VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-10-08 12:02:26 -0400

    Added team element detection into an subsystem and auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2022_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2022_Auto.java
@@ -3,17 +3,21 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Subsystems.TeamElementDetection.TeamElementSubsystem;
+
 @Autonomous(name="FTC_14133_2022_Auto", group="Auto")
 
 public class  FTC_14133_2022_Auto extends LinearOpMode{
 
-    //private Intake Intake=null;
+    public int element_zone = 1;
+
+    private TeamElementSubsystem teamElementDetection=null;
 
     public void HardwareStart() {
         telemetry.addData("Object Creation", "Start");
         telemetry.update();
 
-        //Intake = new Intake(hardwareMap);
+        teamElementDetection = new TeamElementSubsystem(hardwareMap);
 
         telemetry.addData("Object Creation", "Done");
         telemetry.update();
@@ -25,9 +29,14 @@ public class  FTC_14133_2022_Auto extends LinearOpMode{
 
         HardwareStart();
 
+        while (!opModeIsActive() && !isStopRequested()){
+            element_zone = teamElementDetection.ElementDetection(telemetry);
+        }
+
         telemetry.addData("Object", "Passed waitForStart");
 
         telemetry.update();
 
     }
+
 }
