af3cf6bce554b69c56a61b4bd8bddb5721c012a5
VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-10-08 11:36:06 -0400

    Squashed commit of the following:
    commit 817d617f8e0db7e6a4a659b346681de4ecd662bd
    Author: VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
    Date:   Sun Oct 8 11:16:51 2023 -0400
    
        Fixed master
    
    commit 41f929dd058b56dd6ad3afd46a87d5cef525e880
    Author: VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
    Date:   Sun Oct 8 11:16:29 2023 -0400
    
        Put odo tests in reference repo
    
    commit 3c3af23f5e0012e2bb3b976a340da6d1dccf1769
    Author: VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
    Date:   Sun Oct 8 11:03:50 2023 -0400
    
        put reference in FTC-14133-Reference
    
    commit 1aeeb65f5d480beee5b3e2fdd621672df038ebf8
    Author: VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
    Date:   Sun Oct 8 11:03:24 2023 -0400
    
        updated repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2022_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2022_Auto.java
@@ -1,40 +1,19 @@
-
 package org.firstinspires.ftc.teamcode;
-// https://first-tech-challenge.github.io/SkyStone/  This is the link to ALL metered of FTC
-
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
-import org.firstinspires.ftc.teamcode.Subsystems.Intake;
-import org.firstinspires.ftc.teamcode.Subsystems.Lift;
-import org.firstinspires.ftc.teamcode.Subsystems.Sensors;
-import org.firstinspires.ftc.teamcode.Subsystems.Detection;
-
 @Autonomous(name="FTC_14133_2022_Auto", group="Auto")
 
 public class  FTC_14133_2022_Auto extends LinearOpMode{
-    private Drivetrain drivetrain=null; // This activates the sub systems
-    //private Intake Intake=null;
-    //private Lift Lift =null;
-    //private Sensors Sensors=null;
-    //private Detection Detection=null;
-    //private ColorSensorSubsystem Color = null;
-    int routine = 7;
-    int detected = 2;
 
+    //private Intake Intake=null;
 
     public void HardwareStart() {
         telemetry.addData("Object Creation", "Start");
         telemetry.update();
 
-        drivetrain = new Drivetrain(hardwareMap);
         //Intake = new Intake(hardwareMap);
-        //Lift = new Lift(hardwareMap);
-        //Sensors = new Sensors(hardwareMap);
-        //Detection = new Detection(hardwareMap);
-        //Color = new ColorSensorSubsystem(hardwareMap);
 
         telemetry.addData("Object Creation", "Done");
         telemetry.update();
@@ -47,10 +26,6 @@ public class  FTC_14133_2022_Auto extends LinearOpMode{
         HardwareStart();
 
         telemetry.addData("Object", "Passed waitForStart");
-
-        drivetrain.DrivetrainAutoMove(10, 0.5, 0, 0, telemetry);
-        drivetrain.DrivetrainAutoMove(10, 0.5, 90, 0, telemetry);
-        drivetrain.DrivetrainAutoMove(10, 0.5, 45, 0, telemetry);
 
         telemetry.update();
 
