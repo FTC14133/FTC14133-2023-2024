63136a616c627ead41062262d6a0e52a84be7a18
VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-11-26 10:15:26 -0500

    merged back into and moved into folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -4,24 +4,48 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.Roadrunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.Roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.Subsytem.Arm;
+import org.firstinspires.ftc.teamcode.Subsytem.Intake;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(group = "autoFTC14133")
 public class Autonomous extends LinearOpMode {
+
+    private Arm arm=null;
+    private Intake intake=null;
+
+    enum State {
+        SPIKE,
+        BACKDROP,
+        ALIGN,
+        PLACE,
+        PARK,
+        IDLE
+    }
+
+    State currentState = State.IDLE;
+
+    int armSlidePos = -1;
+    double intakeState = 0;
+
+    ElapsedTime intakeTimer = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Creating Drivetrain
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
+        arm = new Arm(hardwareMap);
+        intake = new Intake(hardwareMap);
+
         String[] selectedArray = autoSelector();
 
         String alliance = selectedArray[0];
         String spike = selectedArray[1];
         String side = selectedArray[2];
-        String lrFMode =  selectedArray[3];
 
         double startX = 0;
         double startY = 0;
@@ -32,6 +56,7 @@ public class Autonomous extends LinearOpMode {
                     case "far":
                         startX = -35;
                         startY = -62;
+                        break;
                     case "close":
                         startX = 12;
                         startY = -62;
@@ -41,13 +66,14 @@ public class Autonomous extends LinearOpMode {
                     case "far":
                         startX = -35;
                         startY = 80;
+                        break;
                     case "close":
                         startX = 12;
                         startY = 80;
                 }
         }
 
-        Pose2d startPose = new Pose2d(startX, startY, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(startX, startY, Math.toRadians(0));
 
         drive.setPoseEstimate(startPose);
 
@@ -67,122 +93,129 @@ public class Autonomous extends LinearOpMode {
 
         TrajectorySequence RstraightTo = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
                 .lineToLinearHeading(new Pose2d(40, -36, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(61, -58), 0)
                 .build();
 
-        TrajectorySequence RfarLRB = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-                .lineToConstantHeading(new Vector2d(-36, -36))
-                .lineToConstantHeading(new Vector2d(-36, -11))
-                .lineToLinearHeading(new Pose2d(35, -59, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(40, -36), Math.toRadians(180))
-                .splineToConstantHeading(new Vector2d(61, -58), 0)
-                .build();
-
-        TrajectorySequence RfarLRT = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+        TrajectorySequence RfarLR = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
                 .lineToConstantHeading(new Vector2d(-36, -36))
                 .lineToConstantHeading(new Vector2d(-36, -11))
                 .lineToLinearHeading(new Pose2d(35, -11, Math.toRadians(180)))
                 .splineToConstantHeading(new Vector2d(40, -36), Math.toRadians(180))
+                .build();
+
+
+
+        Trajectory RbackL = drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineToConstantHeading(new Vector2d(40, -30))
+                .build();
+
+        Trajectory RbackC = drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineToConstantHeading(new Vector2d(40, -36))
+                .build();
+
+        Trajectory RbackR = drive.trajectoryBuilder(drive.getPoseEstimate())
+                .lineToConstantHeading(new Vector2d(40, -42))
+                .build();
+
+
+        Trajectory park = drive.trajectoryBuilder(drive.getPoseEstimate())
                 .splineToConstantHeading(new Vector2d(61, -58), 0)
                 .build();
 
 
 
+        currentState = State.SPIKE;
+        switch (spike){
+            case "left":
+                drive.followTrajectoryAsync(RspikeL);
+                break;
+            case "right":
+                drive.followTrajectoryAsync(RspikeR);
+                break;
+            case "center":
+                drive.followTrajectoryAsync(RspikeC);
+                break;
+        }
 
+        while (opModeIsActive() && !isStopRequested()) {
+            switch (currentState){
+                case SPIKE:
+                    if (!drive.isBusy()){
+                        currentState = State.BACKDROP;
 
-        Trajectory BspikeL = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .lineToConstantHeading(new Vector2d(-47, 40))
-                .build();
-        Trajectory BspikeC = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .lineToConstantHeading(new Vector2d(-36, 36))
-                .build();
-        Trajectory BspikeR = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .lineToConstantHeading(new Vector2d(-24, 36))
-                .build();
+                        armSlidePos = 1;
 
-
-
-        TrajectorySequence BstraightTo = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-                .lineToLinearHeading(new Pose2d(40, 36, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(61, 58), 0)
-                .build();
-
-        TrajectorySequence BfarLRB = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-                .lineToConstantHeading(new Vector2d(-36, 36))
-                .lineToConstantHeading(new Vector2d(-36, 11))
-                .lineToLinearHeading(new Pose2d(35, 59, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(40, 36), Math.toRadians(180))
-                .splineToConstantHeading(new Vector2d(61, 58), 0)
-                .build();
-
-        TrajectorySequence BfarLRT = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-                .lineToConstantHeading(new Vector2d(-36, 36))
-                .lineToConstantHeading(new Vector2d(-36, 11))
-                .lineToLinearHeading(new Pose2d(35, 11, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(40, 36), Math.toRadians(180))
-                .splineToConstantHeading(new Vector2d(61, 58), 0)
-                .build();
-
-
-
-        if (!isStopRequested()){
-            switch (alliance){
-                case "red":
-                    switch (spike){
-                        case "left":
-                            drive.followTrajectory(RspikeL);
-                        case "right":
-                            drive.followTrajectory(RspikeR);
-                        case "center":
-                            drive.followTrajectory(RspikeC);
+                        switch (side){
+                            case "close":
+                                drive.followTrajectorySequenceAsync(RstraightTo);
+                                break;
+                            case "far":
+                                switch (spike){
+                                    case "left": case "right":
+                                        drive.followTrajectorySequenceAsync(RfarLR);
+                                        break;
+                                    case "center":
+                                        drive.followTrajectorySequenceAsync(RstraightTo);
+                                        break;
+                                }
+                        }
                     }
-                    switch (side){
-                        case "close":
-                            drive.followTrajectorySequence(RstraightTo);
-                        case "far":
-                            switch (spike){
-                                case "left": case "right":
-                                    switch (lrFMode){
-                                        case "top":
-                                            drive.followTrajectorySequence(RfarLRT);
-                                        case "bottom":
-                                            drive.followTrajectorySequence(RfarLRB);
-                                    }
-                                case "center":
-                                    drive.followTrajectorySequence(RstraightTo);
-                            }
-                    }
+                    break;
 
+                case BACKDROP:
+                    if (!drive.isBusy()){
+                        currentState = State.ALIGN;
 
+                        switch (spike) {
+                            case "left":
+                                drive.followTrajectoryAsync(RbackL);
+                                break;
+                            case "right":
+                                drive.followTrajectoryAsync(RbackR);
+                                break;
+                            case "center":
+                                drive.followTrajectoryAsync(RbackC);
+                                break;
+                        }
+                    }
+                    break;
 
-                case "blue":
-                    switch (spike){
-                        case "left":
-                            drive.followTrajectory(BspikeL);
-                        case "right":
-                            drive.followTrajectory(BspikeR);
-                        case "center":
-                            drive.followTrajectory(BspikeC);
+                case ALIGN:
+                    if (!drive.isBusy()){
+                        currentState = State.PLACE;
+
+                        intakeState = 1;
+                        intakeTimer.reset();
+
                     }
-                    switch (side){
-                        case "close":
-                            drive.followTrajectorySequence(BstraightTo);
-                        case "far":
-                            switch (spike){
-                                case "left": case "right":
-                                    switch (lrFMode){
-                                        case "top":
-                                            drive.followTrajectorySequence(BfarLRT);
-                                        case "bottom":
-                                            drive.followTrajectorySequence(BfarLRB);
-                                    }
-                                case "center":
-                                    drive.followTrajectorySequence(BstraightTo);
-                            }
+                    break;
+
+                case PLACE:
+                    if (intakeTimer.seconds() == 2.0){
+                        intakeState = 0;
+
+                        currentState = State.PARK;
+                        drive.followTrajectoryAsync(park);
                     }
+                    break;
+
+                case PARK:
+                    if (!drive.isBusy()){
+                        currentState = State.IDLE;
+                    }
+                    break;
+
+                case IDLE:
+                    break;
             }
 
+            drive.update();
+
+            arm.GoToPosition(armSlidePos);
+            intake.objcatcher.runIntake(intakeState);
+
+
         }
+
     }
 
     public String[] autoSelector(){
