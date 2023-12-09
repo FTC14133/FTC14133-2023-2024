b66cdb2246cc7eefd8cb8dcb1bfdc2ad4eecf6ba
VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-12-09 17:03:38 -0500

    fixed it

--- MeepMeepTesting/src/main/java/com/example/MeepMeepTesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/MeepMeepTesting/MeepMeepTesting.java
@@ -1,4 +1,4 @@
-package com.example.MeepMeepTesting;
+package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(600);
+        MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
