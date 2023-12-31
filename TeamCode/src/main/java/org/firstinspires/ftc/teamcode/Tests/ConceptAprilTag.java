293a0ff5491736078855b5639fea9f651dc02789
VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-12-30 21:33:01 -0500

    cleaned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ConceptAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ConceptAprilTag.java
@@ -65,6 +65,7 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: AprilTag", group = "Concept")
+@Disabled
 
 public class ConceptAprilTag extends LinearOpMode {
 
